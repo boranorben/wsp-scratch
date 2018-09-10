cf5ef497c4343612c2f02a7412ef22442f19a350
boranorben <zhotkp@gmail.com>
2018-09-10 21:46:06 +0700

    first initialzed with article

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// User Schema
+const UserSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require: true
+        },
+        username: {
+            type: String,
+            require: true
+        },
+        password: {
+            type: String,
+            require: true
+        }
+    }
+)
+
+module.exports = mongoose.model('User', UserSchema);
